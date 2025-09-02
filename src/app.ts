48527178ecc964353e83f1a2782123b05666ab80
Gustavo-Nasc <gustavo.ns@outlook.com.br>
2025-09-02 18:14:34 -0300

    chore: register route

--- src/app.ts
+++ src/app.ts
@@ -19,6 +19,7 @@ import { cofapRoutes } from './controllers/cofap/cofap.routes'
 import { urlFileToBase64 } from './controllers/urlFileToBase64'
 import { eckermannRoutes } from './controllers/eckermann/eckermann.routes'
 import { jsonToStringGemini } from './controllers/jsonToStringGemini'
+import { invistaiiRoutes } from './controllers/invistaii/invistaii.routes'
 
 export const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -43,3 +44,4 @@ app.register(cofapRoutes)
 app.register(urlFileToBase64)
 app.register(eckermannRoutes)
 app.register(jsonToStringGemini)
+app.register(invistaiiRoutes)
