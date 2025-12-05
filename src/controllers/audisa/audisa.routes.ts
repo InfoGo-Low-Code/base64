9e8a8f3c7dde43c4f1314b727340f3d9d7f53827
Gustavo-Nasc <gustavo.ns@outlook.com.br>
2025-12-05 13:55:21 -0300

    chore: register addUsage route from audisa

--- src/controllers/audisa/audisa.routes.ts
+++ src/controllers/audisa/audisa.routes.ts
@@ -3,10 +3,12 @@ import { readExcelData } from './routes/readExcelData'
 import { verifyUsage } from './routes/verifyUsage'
 import { clearUsage } from './routes/clearUsage'
 import { removeUsage } from './routes/removeUserUsage'
+import { addUsage } from './routes/addUserUsage'
 
 export function audisaRoutes(app: FastifyZodTypedInstance) {
   app.register(readExcelData)
   app.register(verifyUsage)
   app.register(clearUsage)
   app.register(removeUsage)
+  app.register(addUsage)
 }
